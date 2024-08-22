f013be534c6d732ca9a5a534dc169e9561b9b6a4
Asadullo961113 <asadillok9@gmail.com>
2024-08-22 09:51:13 +0900

    feat:mongodbga ulanish

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log('web serverni boshlash');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR", err);
+    }
+    else {
+        user = JSON.parse(data);
+    }
+});
+
+// MongoDB chaqirish
+const db = require("./server").db();
+
+// 1 enter code
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 : session code
+// 3 view code
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+// 4 routing code
+app.post("/create-item", (req, res) => {
+    // TODO
+})
+app.get('/author', (req, res) => {
+    res.render("author",{user: user} );
+})
+app.get("/", function(req, res) {
+    res.render("reja");
+});
+
+module.exports = app;
+
+// comment
+
+
+
--- server.js
+++ server.js
@@ -1,47 +1,26 @@
-console.log('web serverni boshlash');
-const express = require('express');
-const app = express();
 const http = require('http');
-const fs = require('fs');
+const mongodb = require('mongodb');
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR", err);
-    }
-    else {
-        user = JSON.parse(data);
-    }
-});
-// 1 enter code
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+let db;
+const connectionString = "mongodb+srv://Asadulloh:0ARlpx4L8BQjFJBu@cluster0.lyocx.mongodb.net/Reja"
 
-// 2 : session code
-// 3 view code
-app.set('views', 'views');
-app.set('view engine', 'ejs');
+mongodb.connect(connectionString,
+    {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+    },
+    (err, client) => {
+        if(err)
+            console.log("error on connection mongodb");
+        else {
+            console.log("mongodb connection succedd");
+            module.exports = client;
 
-// 4 routing code
-app.post("/create-item", (req, res) => {
-    // TODO
-})
-app.get('/author', (req, res) => {
-    res.render("author",{user: user} );
-})
-app.get("/", function(req, res) {
-    res.render("reja");
-});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`this server is running  successfully on port: ${PORT}, http://localhost:${PORT}`);
-})
-
-// comment
-
-
-
-
-
+            const app = require('./app');
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function(){
+                console.log(`this server is running  successfully on port: ${PORT}, http://localhost:${PORT}`);
+            });
+        }
+    });
