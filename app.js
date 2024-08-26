028f4b1390cc3f178b5a883e7041d29e9dcc802c
Asadullo961113 <asadillok9@gmail.com>
2024-08-27 07:20:18 +0900

    fix:done

--- app.js
+++ app.js
@@ -18,16 +18,23 @@ const db = require("./server").db();
 const mongodb = require("mongodb");
 
 // 1 enter code
+// app.use(): Middleware qo'shish uchun ishlatiladi, barcha so'rovlar uchun yoki ma'lum bir yo'nalish uchun.
+// Middleware'lar dasturda so'rovlarni qabul qilib, ularga kerakli ishlov beradi va keyingi jarayonga o'tkazadi.
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // 2 : session code
 // 3 view code
+// SET() => Express.js ilovasining ko'rinishlarini qanday ishlashini va qayerda saqlanishini belgilaydi, 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 // 4 routing code
+// POST() => so'rovlari ma'lumot yuborish yoki yaratish uchun ishlatiladi.
+// req.body: HTTP so'rovining tanasida yuborilgan ma'lumotlarni saqlaydi.
+/* 'req.body.reja:' req.body ob'ekti ichida yuborilgan JSON ma'lumotlardan reja nomli maydonni olish uchun ishlatiladi. 
+ Demak, foydalanuvchi so'rov yuborishda reja maydoniga qiymat kiritgan.*/
 app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
     const new_reja = req.body.reja;
@@ -35,7 +42,6 @@ app.post("/create-item", (req, res) => {
         console.log(data.ops);
         res.json(data.ops[0]);
     });
-    // TODO
 });
 
 app.post("/delete-item", (req, res)=>{
@@ -48,6 +54,27 @@ app.post("/delete-item", (req, res)=>{
     );
 
 });
+
+app.post("/edit-item", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    db.collection("plans")
+    .findOneAndUpdate({_id:new mongodb.ObjectId(data.id)},
+    {$set:{reja: data.new_input}}, function (err, data) {
+        res.json({ state:"success"});
+    }
+);
+})
+
+app.post("/delete-all",(req, res) => {
+    if(req.body.delete_all){
+        db.collection("plans").deleteMany(function(){
+            res.json({ state:"hamma rejalar o'chirildi"});
+        });
+    }
+});
+
+// GET() => so'rovlari web sahifalarni olish uchun ishlatiladi.
 
 app.get('/author', (req, res) => {
     res.render("author",{user: user} );
